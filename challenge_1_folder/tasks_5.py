cab3712dc916152e5bdbfe004c30c12f5f0ee9f3
Shyamal kar <190195736+shyamalkar@users.noreply.github.com>
2026-08-11 19:22:39 +0530

    Update tasks_5.py

--- challenge_1_folder/tasks_5.py
+++ challenge_1_folder/tasks_5.py
@@ -6,6 +6,6 @@ current_year = 2026
 
 age = current_year - birth_year
 
-print("Hello", first_name, last_name + ",", "you are ", age, "years old.")
+print("Hello", first_name, last_name + ",", "you are ", age, "years old.") 
 
 
