3cf8023aba126e8e74422cb1ed0978473cbc9376
Shyamal kar <190195736+shyamalkar@users.noreply.github.com>
2026-08-15 21:45:22 +0530

    Update project.py

--- challenge_1_folder/project.py
+++ challenge_1_folder/project.py
@@ -21,6 +21,7 @@ Everyone was scared untill the brave hero, {hero_name}, arrived.
 
 
 using the magical number {magic_number}, {hero_name} defeated the {animal} and saved {place}.
+
  
 from that day on, the people of {place}
 celebrated the legendary {hero_name} and the amazing {adjective} adventure !
